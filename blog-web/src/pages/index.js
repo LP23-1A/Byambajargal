4adaa1ba737ac84b64560407b9129f8f86f1fa1c
Bymbaabatdelger <145735499+Bymbaabatdelger@users.noreply.github.com>
2023-12-04 14:09:29 +0800

    test

--- blog-web/src/pages/index.js
+++ blog-web/src/pages/index.js
@@ -1,8 +1,17 @@
-
+import Allblogpostvalue from "../../components/AllblogpostValue";
+import TrendingValue from "../../components/TrendingValue";
 
 export default function Home() {
   return (
-    <main>
+    <main className="flex flex-col gap-4 w-screen h-screen items-center justify-center bg-grey">
+      <div className="w-[320px] h-[298px]">
+         <TrendingValue/>
+      </div>
+   
+    <div className="flex">
+       <Allblogpostvalue/>
+    </div>
+   
     </main>
   )
 }
